940121486aa87739f4bed817b82c2a9774b5cc54
hex108 <heixia108@gmail.com>
2011-06-18 15:16:13 +0800

    binary search

---
+++ ch04/binary_search.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+
+//The program in programing pearls 9.3 is perfect!  
+//while the following is just so 
+int binary_search(int *a, int num, int t)
+{
+    int start = 0, end = num - 1;
+    int p = -1;
+    
+    while(end >= start){
+        printf("%d ~ %d\n", start, end);
+        int middle = (start + end) / 2;
+        int tmp = a[middle];
+        if(tmp < t){
+            start = middle + 1;
+        }else if(tmp > t){
+            end = middle - 1;
+        }else{
+            //Version 1: just return an random element that equals "t".
+            return middle;
+
+            //Version 2: find the first element that equals "t".
+            //p = middle;
+            //end = middle - 1;
+        }
+    }
+
+    return p;
+}
+
+int main()
+{
+    int a[] = { 3, 3, 3, 3, 3, 11};
+
+    int m = binary_search(a, 6, 3);
+
+    printf("%d\n", m);
+
+	return 0;
+}
---
+++ ch04/binary_search_opt.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int binary_search(int *a, int num, int t)
+{
+    int low = -1, high = num - 1;
+    
+    //invariant: low < high && a[low] < t && a[high] >= t
+    while(low + 1 != high){
+        //printf("%d ~ %d\n", low, high);
+        int middle = (low + high) / 2;
+        if(a[middle] < t){
+            low = middle;
+        }else{
+            high = middle;
+        }
+    }
+    
+    //assert low +1 = high && a[low] < t && a[high] >= t
+    if(a[high] != t)
+        return -1;
+    else
+        return high;
+}
+
+int int_cmp(const int *a, const int *b)
+{
+    return (*a - *b);
+}
+
+int main()
+{
+    int a[] = { 3, 3, 3, 3, 3, 11};
+
+    int m = binary_search(a, 6, 3);
+    
+    printf("%d\n", m);
+    
+    int b = 3;
+    int *n = bsearch(&b, a, 6, sizeof(int), int_cmp);
+    
+    printf("%d\n", n -  a);
+
+	return 0;
+}
